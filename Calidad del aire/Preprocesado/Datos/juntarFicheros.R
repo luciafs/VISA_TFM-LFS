c8730d368174a29629b12725fdcbf6c7dd27c0f7
luciafs <40027258+luciafs@users.noreply.github.com>
2018-07-24 16:48:17 +0200

    header

--- Calidad del aire/Preprocesado/Datos/juntarFicheros.R
+++ Calidad del aire/Preprocesado/Datos/juntarFicheros.R
@@ -1,4 +1,8 @@
 
+#	Autor: Lucia Fernandez Sanchez                 
+#	TFM Master Visual Analytics and Big Data     									
+#	Universidad Internacional de La Rioja (UNIR)   									
+
 
 library(data.table)
 datos = fread("Anio2001.csv")
@@ -10,11 +14,11 @@ for(anio in seq(2002,2018,1)){
 }
 
 # Vamos a quedarnos unicamente con el estudio de 3 estaciones de control, 1 por cada tipo:
-# UT (urbana de trafico): 8; Escuelas Aguirre; Entre C/ Alcal· y C/ O' Donell 
+# UT (urbana de trafico): 8; Escuelas Aguirre; Entre C/ Alcal√° y C/ O' Donell 
 # UF (urbana de fondo): 18; Farolillo; Calle Farolillo - C/Ervigio
-# S (suburbana): 24;	Casa de Campo; Casa de Campo (Terminal del TelefÈrico)
+# S (suburbana): 24;	Casa de Campo; Casa de Campo (Terminal del Telef√©rico)
 # ATENCION: Las anteriores son las unicas estaciones que recogen mediciones de todos los contaminantes de interes
-# y ademas desde el aÒo 2001
+# y ademas desde el a√±o 2001
 
 datos = datos[which(datos$cod_est=="28079008" |
                     datos$cod_est=="28079018" |
