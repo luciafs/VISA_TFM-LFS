fbcf30021715d7a6c0f7b409a5d7943f1aae1efa
luciafs <40027258+luciafs@users.noreply.github.com>
2018-07-24 16:46:29 +0200

    header

--- Calidad del aire/Datos en tiempo real/download_RT.R
+++ Calidad del aire/Datos en tiempo real/download_RT.R
@@ -1,4 +1,8 @@
 
+#	Autor: Lucia Fernandez Sanchez                 
+#	TFM Master Visual Analytics and Big Data     									
+#	Universidad Internacional de La Rioja (UNIR)   									
+
 url_txt <- "http://www.mambiente.munimadrid.es/opendata/horario.txt"
 download.file(url_txt, paste0("Datos/",Sys.Date(),".txt"))
 
